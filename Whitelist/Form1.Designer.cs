752775bc0d3d687970414807eaf029942e46b911
Verfired_ <71733945+Verfired@users.noreply.github.com>
2021-01-09 21:14:19 +0300

    Add files via upload

---
+++ Whitelist/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Whitelist
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.email = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(67, 113);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(267, 31);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // email
+            // 
+            this.email.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.email.Location = new System.Drawing.Point(45, 87);
+            this.email.Name = "email";
+            this.email.Size = new System.Drawing.Size(304, 13);
+            this.email.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(398, 201);
+            this.Controls.Add(this.email);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox email;
+    }
+}
+
---
+++ Whitelist/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Whitelist
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string whitelist = new System.Net.WebClient() { Proxy = null }.DownloadString("https://pastebin.com/raw/0ysn9yFw");//Whitelist 
+            string blacklist = new System.Net.WebClient() { Proxy = null }.DownloadString("https://pastebin.com/raw/QdBStAPz");//Blacklisted
+            if (blacklist.Contains(email.Text))
+            {
+                MessageBox.Show("Blacklisted! Closing...");
+                Application.Exit();
+            }
+            if (whitelist.Contains(email.Text))
+            {
+                MessageBox.Show("Whitelisted!");
+                Form2 frm2 = new Form2();
+                frm2.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Put valid key bruhh.");
+            }
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
